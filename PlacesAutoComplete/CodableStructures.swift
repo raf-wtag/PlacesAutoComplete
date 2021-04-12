becd7e8341f93475f5bf40be5a1677f47d3cbbd9
raf-wtag <fahim.rahman@welldev.io>
2021-04-12 20:13:35 +0600

    SecretKey Parse Complete
    Mapbox api key stored in Keys.json file

--- PlacesAutoComplete/CodableStructures.swift
+++ PlacesAutoComplete/CodableStructures.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct SecretKeysMap: Codable {
+    let APIKEY: String
+}
+
--- PlacesAutoComplete/PlacesAutoCompleteVC.swift
+++ PlacesAutoComplete/PlacesAutoCompleteVC.swift
@@ -6,16 +6,95 @@
 //
 
 import UIKit
+import CoreLocation
 
-class PlacesAutoCompleteVC: UIViewController {
+class PlacesAutoCompleteVC: UIViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var isSearchActive = false
+    var mapbox_api = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+    var mapbox_access_token = ""
+    var secretKeyContainFile = "Keys.json"
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Parse secret API key from Keys.json
+        if let rawAPIKeyData = self.readSecretKeyFromFile(forFileName: secretKeyContainFile) {
+            if let retrivedKey = self.parseSecretKeyFile(jsonData: rawAPIKeyData) {
+                self.mapbox_access_token = retrivedKey
+                print(self.mapbox_access_token)
+            }
+        }
     }
     
+    // MARK: Parse secret API key from Keys.json
+    func readSecretKeyFromFile(forFileName name: String) -> Data? {
+        do {
+            if let bundlePath = Bundle.main.path(forResource: name, ofType: "json"), let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                return jsonData
+            }
+        } catch {
+            print("API KEY LOADING FAILED WITH ERROR", error)
+        }
+        return nil
+    }
+    
+    private func parseSecretKeyFile(jsonData: Data) -> String? {
+        do {
+            let decodedSecretKeys = try JSONDecoder().decode(SecretKeysMap.self, from: jsonData)
+            print("API key is", decodedSecretKeys.APIKEY)
+            return decodedSecretKeys.APIKEY
+        } catch {
+            print("Hey!! Error in Decoding!!")
+        }
+        return nil
+    }
 
-
+    
+    // MARK: SearchBar Delegate Functions
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.cancelSearching()
+        self.isSearchActive = false
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
+    func cancelSearching() {
+        isSearchActive = false
+        self.searchBar.resignFirstResponder()
+        self.searchBar.text = ""
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(self.searchPlacesSuggestion), object: nil)
+        
+        self.perform(#selector(self.searchPlacesSuggestion), with:nil, afterDelay: 0.5)
+        
+        if((searchBar.text?.isEmpty) != nil) {
+            isSearchActive = false
+        } else {
+            isSearchActive = true
+        }
+    }
+    
+    @objc func searchPlacesSuggestion() {
+        
+    }
+    
 }
