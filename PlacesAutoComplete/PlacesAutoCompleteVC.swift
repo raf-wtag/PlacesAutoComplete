4a82ebb4b1b79efd4998e8dc849f8993cfa548a4
raf-wtag <fahim.rahman@welldev.io>
2021-04-12 19:36:39 +0600

    Project Organized
    PlacesAutoCompleteVC add and ViewController deleted
    Added a searchBar and TableView and its outlet

---
+++ PlacesAutoComplete/PlacesAutoCompleteVC.swift
@@ -0,0 +1,21 @@
+//
+//  PlacesAutoCompleteVC.swift
+//  PlacesAutoComplete
+//
+//  Created by Fahim Rahman on 12/4/21.
+//
+
+import UIKit
+
+class PlacesAutoCompleteVC: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+}
