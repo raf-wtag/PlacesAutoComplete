5a0fcb72de4c145a4983b9c00de894c5544927cb
raf-wtag <fahim.rahman@welldev.io>
2021-04-18 00:54:33 +0600

    HomeViewController Added

---
+++ PlacesAutoComplete/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  PlacesAutoComplete
+//
+//  Created by Fahim Rahman on 18/4/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
